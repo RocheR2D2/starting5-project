398ac01916193b1eedd26a1a1d342681e83d001f
Tony Eung <eungtony@outlook.fr>
2017-10-29 19:38:41 +0100

    Override Registration Form

---
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Form;
+
+use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'Pseudo', 'translation_domain' => 'FOSUserBundle'))
+            ->add('firstname', null, array('label' => 'Firstname', 'translation_domain' => 'FOSUserBundle'))
+            ->add('lastname', null, array('label' => 'Lastname', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Confirm Password'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+        ;
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_token_id' => 'Registration',
+            // BC for SF < 2.8
+            'intention' => 'Registration',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'starting5_fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
