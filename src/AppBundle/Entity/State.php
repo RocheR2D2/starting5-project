29aaea2a485bc3a0979ecd813609341c1d42dbc4
Tony Eung <eungtony@outlook.fr>
2017-10-31 19:33:50 +0100

    Create State Entity

---
+++ src/AppBundle/Entity/State.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * State
+ *
+ * @ORM\Table(name="state")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\StateRepository")
+ */
+class State
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug_state", type="string", length=255)
+     */
+    private $slugState;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return State
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set slugState
+     *
+     * @param string $slugState
+     *
+     * @return State
+     */
+    public function setSlugState($slugState)
+    {
+        $this->slugState = $slugState;
+
+        return $this;
+    }
+
+    /**
+     * Get slugState
+     *
+     * @return string
+     */
+    public function getSlugState()
+    {
+        return $this->slugState;
+    }
+}
+
