1ca10c645ceaea470dec2f9eaad673af2bdf8ea9
Tony Eung <eungtony@outlook.fr>
2017-12-08 10:33:48 +0100

    Add hint

--- src/AppBundle/Controller/PackController.php
+++ src/AppBundle/Controller/PackController.php
@@ -4,9 +4,9 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\NBAPlayers;
 use AppBundle\Entity\UsersPlayers;
-use AppBundle\Entity\UserTeam;
-use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -22,23 +22,48 @@ class PackController extends Controller
         $userPlayerRepository = $this->getDoctrine()->getRepository(UsersPlayers::class);
         $userPlayers = $userPlayerRepository->findBy(['userId' => $this->getUser()]);
         $countPlayers = count($userPlayers) + 3;
-        $form = $this->createFormBuilder()
-            ->add('save', SubmitType::class, array('label' => 'OPEN A PACK'))
+        $silverForm = $this->createFormBuilder()
+            ->add('save', SubmitType::class, array('label' => 'Silver Pack'))
+            ->add('type', HiddenType::class, array(
+                'data' => 'silver',
+            ))
+            ->getForm();
+        $goldenForm = $this->createFormBuilder()
+            ->add('save', SubmitType::class, array('label' => 'Golden Pack'))
+            ->add('type', HiddenType::class, array(
+                'data' => 'golden',
+            ))
             ->getForm();
 
-        $form->handleRequest($request);
+        $silverForm->handleRequest($request);
+        $goldenForm->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($silverForm->isSubmitted() || $goldenForm->isSubmitted()) {
+            $hint = [];
             $userPlayers = $userPlayerRepository->findBy(['userId' => $this->getUser()->getId()]);
             $playersIds = [];
             foreach ($userPlayers as $userPlayer) {
                 $playersIds[] = $userPlayer->getPlayerId()->getPlayerId();
             }
-            $packContent = $player->packOpener($this->getUser());
+            $type = null;
+            if($silverForm->isSubmitted()){
+                if(isset($silverForm->getData()['type'])){
+                    $type = $silverForm->getData()['type'];
+                }
+            } elseif($goldenForm->isSubmitted()){
+                if(isset($goldenForm->getData()['type'])){
+                    $type = $goldenForm->getData()['type'];
+                }
+            }
+            $packContent = $player->packOpener($type);
 
             foreach ($packContent as $content) {
                 $user = $this->getUser();
-                $user->setQuizPoints($user->getQuizPoints() - 50);
+                if($type == 'golden'){
+                    $user->setQuizPoints($user->getQuizPoints() - 50);
+                } elseif($type == 'silver'){
+                    $user->setQuizPoints($user->getQuizPoints() - 60);
+                }
                 $em = $this->getDoctrine()->getManager();
                 $userPlayer = new UsersPlayers();
                 $nbaPlayer = $player->findOneBy(['playerId' => $content->playerId]);
@@ -52,18 +77,31 @@ class PackController extends Controller
                 $userPlayer->setUserId($this->getUser());
                 $userPlayer->setPosition($nbaPlayer->getPosition());
                 $userPlayer->setRating($nbaPlayer->getRating());
+                if($nbaPlayer->getRating() > 85 && $nbaPlayer->getRating() <= 87){
+                    $hint['rare'] = $nbaPlayer;
+                } else if ($nbaPlayer->getRating() > 87 && $nbaPlayer->getRating() <= 92) {
+                    $hint['very_rare'] = $nbaPlayer;
+                } else if ($nbaPlayer->getRating() > 92 && $nbaPlayer->getRating() <= 94) {
+                    $hint['ultra_rare'] = $nbaPlayer;
+                } else if ($nbaPlayer->getRating() >= 95) {
+                    $hint['epic'] = $nbaPlayer;
+                }
 
                 $em->persist($user);
                 $em->persist($userPlayer);
                 $em->flush();
             }
+
             return $this->render('starting5/pack/pack.html.twig', [
                 'packContent' => $packContent,
+                'hint' => $hint,
+                'type' => $type
             ]);
         }
 
         return $this->render('starting5/pack/index.html.twig', [
-            'form' => $form->createView(),
+            'silverForm' => $silverForm->createView(),
+            'goldenForm' => $goldenForm->createView(),
             'count' => $countPlayers
         ]);
     }
