5dc02cefec0664702f7c98d92777d5322116c7d2
Tony Eung <eungtony@outlook.fr>
2018-03-11 14:59:01 +0100

    create const for PACK

---
+++ src/AppBundle/Helper/Pack.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Helper;
+
+class Pack {
+    const SILVER_PACK_PRICE = 300;
+    const UNIQ_SILVER_PLAYER = 100;
+
+    const GOLDEN_PACK_PRICE = 1500;
+    const UNIQ_GOLDEN_PLAYER = 500;
+
+    const GIGA_PACK_PRICE = 3500;
+    const UNIQ_GIGA_PLAYER = 1500;
+
+    const SUPER_RARE_PACK_PRICE = 10500;
+    const UNIQ_SUPER_RARE_PLAYER = 3500;
+
+    const ULTRA_RARE_PACK_PRICE = 20000;
+
+    const SILVER_PACK_LABEL = 'silver';
+    const GOLDEN_PACK_LABEL = 'golden';
+    const SUPER_RARE_PACK_LABEL = 'super-rare';
+    const GIGA_PACK_LABEL = 'giga';
+
+    const MINIMUM_RARE_RATING = 85;
+    const MAXIMUM_RARE_RATING = 87;
+    const MINIMUM_ULTRA_RARE_RATING = 90;
+    const MAXIMUM_ULTRA_RARE_RATING = 94;
+    const EPIC_RATING = 95;
+
+    const UNIQ_REFUND_PLAYER = 50;
+}
