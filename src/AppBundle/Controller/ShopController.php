f986bb50def99c2470d8ee949151263c2f188886
Tony Eung <eungtony@outlook.fr>
2018-04-30 18:49:48 +0200

    upgrade shop system

--- src/AppBundle/Controller/ShopController.php
+++ src/AppBundle/Controller/ShopController.php
@@ -46,18 +46,28 @@ class ShopController extends Controller
 
     public function buyPlayersAction(Request $request)
     {
+        $user = $this->getUser();
         $playerId = $request->request->get('playerId');
         $player = $this->nbaPlayers->find($playerId);
-        $this->addPlayer($player);
-        $response = new Response($this->shopPlayerCard($player));
+        $shopPlayer =  $this->shopPlayer->findOneBy(['playerId' => $player]);
+        $this->addPlayer($player, $shopPlayer);
+        $content = ['shopPlayer' => $this->shopPlayerCard($player, $shopPlayer), 'points' => $user->getQuizPoints(), 'lastPlayers' => $this->getLastPlayers()];
+        $response = new Response(json_encode($content));
+        $response->headers->set('Content-Type', 'application/json');
 
         return $response;
     }
 
-    public function shopPlayerCard($player)
+    public function getLastPlayers()
+    {
+        $lastPlayers = $this->userPlayers->findBy([], ['id' => 'DESC'], 5);
+
+        return $this->renderView('starting5/players/lastPlayers.html.twig', ['lastPlayers' => $lastPlayers]);
+    }
+
+    public function shopPlayerCard($player, $shopPlayer)
     {
         $userPlayer = $this->userPlayers->findBy(['userId' => $this->getUser(), 'playerId' => $player]);
-        $shopPlayer = $this->shopPlayer->findOneBy(['playerId' => $player]);
         if(!empty($userPlayer)) {
             $shopPlayer->isMyPlayer = empty($userPlayer);
         }
@@ -69,15 +79,19 @@ class ShopController extends Controller
         ]);
     }
 
-    public function addPlayer($player)
+    public function addPlayer($player, $shopPlayer)
     {
+        $user = $this->getUser();
         $userPlayer = new UsersPlayers();
         $userPlayer->setPlayerId($player);
-        $userPlayer->setUserId($this->getUser());
+        $userPlayer->setUserId($user);
         $userPlayer->setPosition($player->getPosition());
         $userPlayer->setRating($player->getRating());
 
+        $user->setQuizPoints($user->getQuizPoints() - $shopPlayer->price);
+
         $this->em->persist($userPlayer);
+        $this->em->persist($user);
         $this->em->flush();
     }
 }
